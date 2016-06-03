98e71504c357722cac615961b90d34024e6c0282
gr8mao <gr8mao@gmail.com>
2016-06-03 22:37:13 +0300

    BooksController started

--- config/routes.php
+++ config/routes.php
@@ -11,5 +11,5 @@ return array(
     'articles/submit' => 'article/submit', // actionSubmit в ArticleController
     'articles' => 'article/index', //actionIndex в ArticleController
     'about' => 'home/about', // actionAbout в HomeController
-    'home' => 'home/index' // actionIndex в HomeController
+    '' => 'home/index' // actionIndex в HomeController
 );
